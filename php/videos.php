166a50c7c000159d58331b685f2d34ff9ac5377c
Pornflix <saigefortrice@gmail.com>
2015-06-06 03:08:25 +1000

    Transferring to loonix master race

--- php/videos.php
+++ php/videos.php
@@ -7,7 +7,7 @@
         $xml = new SimpleXMLElement($xmlstring);
         $length = count($xml->row);
 
-        for($i = 0; $i < $length; $i++) {
+        for($i = 0; $i < ($length-1); $i++) {
             echo "          <li><div id=\"preview\"><img src=\"images/" . $xml->row[$i]['id'] . ".jpg\" height=\"140.625px\" width=\"250px\"></div><p>" . $xml->row[$i]['name'] . "</p></li>";
         }
     }
