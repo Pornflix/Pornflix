49c9ef2be050fcb884cdb50176811695c1ccd414
Pornflix <officialpornflix@gmail.com>
2016-08-28 16:30:20 +1000

    added beta message

--- src/lib/views/Splash.php
+++ src/lib/views/Splash.php
@@ -117,7 +117,8 @@ class Splash {
 	}
 
 	function splashFooter() {
-		$content = "<script>
+		$content = "<span id=\"beta-banner\">[This site is in very early ALPHA, so a lot of the features haven't been implemented yet. Feel free to enjoy the free porn though]</span>";
+		$content .= "<script>
 					(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
